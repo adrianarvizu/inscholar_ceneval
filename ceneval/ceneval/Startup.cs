905eff8e922dfc45a2aaf40c57ceb352d7bd679c
Bryan <pvj2013170@hotmail.com>
2020-01-24 19:48:13 -0600

    subir proyecto ceneval

---
+++ ceneval/ceneval/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ceneval.Startup))]
+namespace ceneval
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
